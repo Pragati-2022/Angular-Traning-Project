86d729ff9f4dfd0d6c433565068bf3f1715f48b9
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-14 18:55:09 +0530

    put productService.id in constructor for show only home page's child compoenet

--- src/app/modules/product/home/product-detail/product-detail.component.ts
+++ src/app/modules/product/home/product-detail/product-detail.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/core/services/product.service';
-import { ToasterService } from 'src/app/core/services/toastr.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss'],
+  styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   product: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private productService: ProductService,
     private router: Router,
-  ) {}
+  ) {
+    const routeParam = this.activatedRoute.snapshot.paramMap;
+    this.productService.id = Number(routeParam.get('id'));
+  }
 
   ngOnInit(): void {
     const routeParam = this.activatedRoute.snapshot.paramMap;
-    console.log(routeParam);
-
     const productIdFromRoute = Number(routeParam.get('id'));
-    console.log(productIdFromRoute);
-
-    this.product = this.productService.products.find(
-      (product) => product.id === productIdFromRoute
-    );
-    console.log(this.product);
+    this.product = this.productService.products.find((product) => product.id === productIdFromRoute);
   }
 
   onBack() {
     this.router.navigate(['home']);
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy');
     this.productService.id = 0;
   }
 }

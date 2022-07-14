7d0972caab35ee8e0cfa27a7f73a3d3af7eaa4ab
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-14 18:59:16 +0530

    added toastar dynamic message and password show hide logic

--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonService } from 'src/app/core/services/common/common.service';
+import { ToasterService } from 'src/app/core/services/toastr.service';
 
 @Component({
   selector: 'app-login',
@@ -13,6 +15,7 @@ export class LoginComponent {
   // });
   loginForm!: FormGroup;
   isFormSubmitted = false;
+  fieldTextType!: boolean;
 
   formData: any[] = [
     { label: 'email', type: 'email',lang:'LABEL.EMAIL'},
@@ -25,6 +28,8 @@ export class LoginComponent {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
+    private toasterService : ToasterService,
+    private commomService : CommonService
   ) {
     this.initialization();
   }
@@ -43,15 +48,19 @@ export class LoginComponent {
   onSubmit() {
     this.isFormSubmitted = true;
     if (!this.loginForm.valid) {
-      alert('Invalid data!');
-      return false;
+      this.toasterService.toasterError(this.commomService.getTranslateData("MESSAGE.ERROR_INVALID"))
+      return
     }
 
     localStorage.setItem('loggedIn', btoa(this.loginForm.value.email));
+    this.toasterService.toasterSuccess(this.commomService.getTranslateData("MESSAGE.SUCCESS_LOGGIN"));
     this.router.navigate(['home']);
 
     this.initialization();
     this.isFormSubmitted = false;
-    return true;
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
   }
 }

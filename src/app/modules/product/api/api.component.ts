3a8f1f5341b5dcf64291bb8a2ef915447ef47f60
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-14 18:52:36 +0530

    added loader

--- src/app/modules/product/api/api.component.ts
+++ src/app/modules/product/api/api.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
-import { Subscription } from 'rxjs';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ApiService } from 'src/app/core/services/product/api.service';
 import { IProduct } from '../../shared/model/productAPI';
 
@@ -10,41 +10,26 @@ import { IProduct } from '../../shared/model/productAPI';
   templateUrl: './api.component.html',
   styleUrls: ['./api.component.scss']
 })
-export class APIComponent implements OnInit, OnDestroy {
+export class APIComponent implements OnInit {
   products!: IProduct[];
   returnedArray!: IProduct[];
-  myValueSub!: Subscription;
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) {
-    const arr = [
-      { place: 'here', name: 'x', other: 'other stuff1' },
-      { place: 'there', name: 'x', other: 'other stuff2' },
-      { place: 'here', name: 'y', other: 'other stuff4' },
-      { place: 'here', name: 'z', other: 'other stuff5' }
-    ];
-
-    console.log(arr.map((data) => data.name));
-    
-
-    function getUniqueListBy(arr: { place: string; name: string; other: string }[], key: string) {
-      return [...new Map(arr.map((item: any) => [item[key], item])).values()];
-    }
-
-    const arr1 = getUniqueListBy(arr, 'place');
-
-    console.log('Unique by place');
-    console.log(JSON.stringify(arr1));
-  }
+  constructor(
+    private apiService: ApiService,
+    private ngxUiLoaderService: NgxUiLoaderService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data: any) => {
       console.log(data['data']);
     });
 
-    this.myValueSub = this.apiService.getProduct().subscribe((data) => {
+    this.ngxUiLoaderService.start();
+    this.apiService.getProduct().subscribe((data) => {
       this.products = data.data;
-
       this.returnedArray = this.products.slice(0, 3);
+      this.ngxUiLoaderService.stop();
     });
   }
 
@@ -52,12 +37,5 @@ export class APIComponent implements OnInit, OnDestroy {
     const startItem = (event.page - 1) * event.itemsPerPage;
     const endItem = event.page * event.itemsPerPage;
     this.returnedArray = this.products.slice(startItem, endItem);
-  }
-
-  ngOnDestroy(): void {
-    if (this.myValueSub) {
-      this.myValueSub.unsubscribe();
-      console.log('unsubcribe');
-    }
   }
 }
